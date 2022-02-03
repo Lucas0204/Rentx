7a022a3fb0a1de2669a54ca6c504cc00ef8cffa1
Lucas Zordan <lucaszordan0204@gmail.com>
2022-02-03 16:23:49 -0300

    introducing documentation to the API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { router } from './routes';
+import swagger from 'swagger-ui-express';
+import swaggerConfig from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerConfig));
 
 app.listen(3333, () => console.log('Server running...'));
