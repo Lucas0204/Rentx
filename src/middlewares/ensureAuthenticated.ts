783ed9a920e724a398b37fd2f808fcf28063489d
Lucas Zordan <lucaszordan0204@gmail.com>
2022-02-07 22:28:20 -0300

    fixing 'ensure authenticated' middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+    sub: string;
+}
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authToken = request.headers.authorization;
@@ -13,8 +18,18 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const token = authToken.split(' ')[1];
 
     try {
-        const { sub: user_id } = verify(token, process.env.JWT_SECRET);
+        const { sub: user_id } = verify(token, process.env.JWT_SECRET) as IPayload;
         
+        const usersRepository = new UsersRepository();
+
+        const user = await usersRepository.findById(user_id);
+
+        if (!user) {
+            return response.status(401).json({
+                error: 'User does not exists!'
+            });
+        }
+
         return next();
     } catch {
         return response.status(401).json({
